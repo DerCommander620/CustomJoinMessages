56d71dcd1eb6b3faaa156e471aa4b38fb53deae5
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-03-26 13:38:02 +0200

    Update Main.php

--- src/DerCommander610/JMSG/Main.php
+++ src/DerCommander610/JMSG/Main.php
@@ -15,7 +15,7 @@ class Main extends PluginBase implements Listener{
     public function onEnable(): void {
     }
     public function onJoinPlayer(PlayerJoinEvent $event) {
-        $event->setMessage("§aDer Spieler " . $event->getPlayer()->getName() . "ist online gegangen");
+        $event->setJoinMessage("§aDer Spieler " . $event->getPlayer()->getName() . "ist online gegangen");
         @mkdir($this->getDataFolder());
         $this->getResource("config.yml");
         if ($sender->setJoinMessage($this->getConfig()->get("JoinMessage"))){
