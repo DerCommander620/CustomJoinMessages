c7167598570c006ac7b4bcf1c21d00ef90395a20
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-03-26 13:22:39 +0200

    Update Main.php

--- src/DerCommander610/JMSG/Main.php
+++ src/DerCommander610/JMSG/Main.php
@@ -18,7 +18,7 @@ class Main extends PLuginBase implements Listener{
     public function onJoinPlayer(PlayerJoinEvent $event) {
         $event->setMessage("§aDer Spieler " . $event->getPlayer()->getName() . "ist online gegangen");
         @mkdir($this->getDataFolder());
-        $this->getResource("config.yml")
+        $this->getResource("config.yml");
         if($sender->setJoinMessage($this->getConfig()->get("JoinMessage")))
     }
 
